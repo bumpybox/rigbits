80db52efe68a673ce2d6581230effb82f94e8cf4
Toke Jepsen <tokejepsen@gmail.com>
2019-05-23 14:19:48 +0100

    Shrinkwrap GUI

--- scripts/mgear/rigbits/mesh_rigger/shrinkwrap_rigger.py
+++ scripts/mgear/rigbits/mesh_rigger/shrinkwrap_rigger.py
@@ -1,12 +1,19 @@
+from functools import partial
+import json
+
 import pymel.core as pm
+from maya.app.general.mayaMixin import MayaQWidgetDockableMixin
 
 from mgear.core import skin, curve, icon
 from . import lib
+from mgear.rigbits import facial_rigger
+from mgear.vendor.Qt import QtCore, QtWidgets
+import mgear.core.pyqt as gqt
 
 
-def shrinkwrap_rig(*args, **kwargs):
+def rig(*args, **kwargs):
     kwargs, organization_keys = lib.extract_organization_keys(kwargs)
-    results = _shrinkwrap_rig(*args, **kwargs)
+    results = _rig(*args, **kwargs)
     lib.organize_results(results, **organization_keys)
 
     # Dont renaming master control
@@ -14,19 +21,23 @@ def shrinkwrap_rig(*args, **kwargs):
     for node in results["controls_set"]:
         if not node.name().endswith("_master_ctrl"):
             nodes.append(node)
+
+    prefix = ""
+    if "prefix" in kwargs:
+        prefix = kwargs["prefix"] + "_"
     lib.rename_by_position(
-        nodes, prefix=kwargs["prefix"] + "_", suffix="_ctrl"
+        nodes, prefix=prefix, suffix="_ctrl"
     )
 
 
-def _shrinkwrap_rig(mesh,
-                    main_start,
-                    main_frequency,
-                    shrinkwrap_mesh,
-                    prefix="shrinkwrap_rig",
-                    hook_up_parent=None,
-                    size=1.0,
-                    offset=0.0):
+def _rig(mesh=None,
+         shrinkwrap_mesh=None,
+         main_control_start=0,
+         main_control_frequency=1,
+         prefix="shrinkwrap_rig",
+         hook_up_parent=None,
+         size=1.0,
+         offset=0.0):
 
     results = {"setup_group": [], "controls_group": [], "controls_set": []}
 
@@ -104,12 +115,12 @@ def _shrinkwrap_rig(mesh,
 
     # Create controls with parent and children. Relationship is determined by
     # skipping edges in the ring. Starting point is configurable.
-    if main_start / main_frequency == 1:
-        main_start = 0
+    if main_control_start / main_control_frequency == 1:
+        main_control_start = 0
 
     # Parent controls
     parent_controls = []
-    for joint in joints[main_start::main_frequency]:
+    for joint in joints[main_control_start::main_control_frequency]:
         group = pm.group(
             name="{0}_main{1:0>2}_grp".format(prefix, joints.index(joint)),
             empty=True
@@ -142,7 +153,7 @@ def _shrinkwrap_rig(mesh,
     # Duplicate the parent controls to loop back around.
     parents = parent_controls + parent_controls
     for joint in joints:
-        if joint in joints[main_start::main_frequency]:
+        if joint in joints[main_control_start::main_control_frequency]:
             parent_index += 1
             continue
 
@@ -170,7 +181,9 @@ def _shrinkwrap_rig(mesh,
         control.resetFromRestPosition()
 
         pm.parentConstraint(control, joint)
-        weight = parent_index - (float(joints.index(joint)) / main_frequency)
+        weight = parent_index - (
+            float(joints.index(joint)) / main_control_frequency
+        )
         pm.parentConstraint(
             parents[parent_index],
             group,
@@ -200,3 +213,135 @@ def _shrinkwrap_rig(mesh,
     master_control.wobble_smooth.set(2)
 
     return results
+
+
+class ui(MayaQWidgetDockableMixin, QtWidgets.QDialog):
+
+    valueChanged = QtCore.Signal(int)
+
+    def __init__(self, parent=None):
+        super(ui, self).__init__(parent)
+
+        self.filter = (
+            "Shrinkwrap Rigger Configuration .shrinkwrap (*.shrinkwrap)"
+        )
+
+        self.create()
+
+    def create(self):
+
+        self.setWindowTitle("Shrinkwrap Rigger")
+        self.setWindowFlags(QtCore.Qt.Window)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose, 1)
+
+        self.create_layout()
+        self.create_connections()
+
+    def create_layout(self):
+
+        main_layout = QtWidgets.QVBoxLayout()
+
+        # Geometry input controls
+        self.mesh_label = QtWidgets.QLabel("Mesh:")
+        self.mesh = QtWidgets.QLineEdit()
+        self.mesh_button = QtWidgets.QPushButton("<<")
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(self.mesh_label)
+        layout.addWidget(self.mesh)
+        layout.addWidget(self.mesh_button)
+        main_layout.addLayout(layout)
+
+        self.shrinkwrap_mesh_label = QtWidgets.QLabel("Shrinkwrap Mesh:")
+        self.shrinkwrap_mesh = QtWidgets.QLineEdit()
+        self.shrinkwrap_mesh_button = QtWidgets.QPushButton("<<")
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(self.shrinkwrap_mesh_label)
+        layout.addWidget(self.shrinkwrap_mesh)
+        layout.addWidget(self.shrinkwrap_mesh_button)
+        main_layout.addLayout(layout)
+
+        self.main_control_start_label = QtWidgets.QLabel("Main Control Start:")
+        self.main_control_start = QtWidgets.QSpinBox()
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(self.main_control_start_label)
+        layout.addWidget(self.main_control_start)
+        main_layout.addLayout(layout)
+
+        self.main_control_frequency_label = QtWidgets.QLabel(
+            "Main Control Frequency:"
+        )
+        self.main_control_frequency = QtWidgets.QSpinBox()
+        self.main_control_frequency.setMinimum(1)
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(self.main_control_frequency_label)
+        layout.addWidget(self.main_control_frequency)
+        main_layout.addLayout(layout)
+
+        self.build_button = QtWidgets.QPushButton("Build")
+        main_layout.addWidget(self.build_button)
+
+        self.import_button = QtWidgets.QPushButton("Import Config From Json")
+        main_layout.addWidget(self.import_button)
+
+        self.export_button = QtWidgets.QPushButton("Export Config To Json")
+        main_layout.addWidget(self.export_button)
+
+        self.setLayout(main_layout)
+
+    def create_connections(self):
+        self.mesh_button.clicked.connect(
+            partial(self.populate_object, self.mesh)
+        )
+        self.shrinkwrap_mesh_button.clicked.connect(
+            partial(self.populate_object, self.shrinkwrap_mesh)
+        )
+
+        self.build_button.clicked.connect(self.build_rig)
+        self.import_button.clicked.connect(self.import_settings)
+        self.export_button.clicked.connect(self.export_settings)
+
+    def populate_object(self, line_edit):
+        selection = pm.selected()
+        if selection:
+            if len(selection) > 1:
+                pm.displayWarning(
+                    "Selected more and one object."
+                    " Getting first selected object."
+                )
+
+            line_edit.setText(selection[0].name())
+        else:
+            pm.displayWarning("No object selected.")
+
+    def build_rig(self):
+        rig(**facial_rigger.lib.get_settings_from_widget(self))
+
+    def export_settings(self):
+        data_string = json.dumps(
+            facial_rigger.lib.get_settings_from_widget(self),
+            indent=4,
+            sort_keys=True
+        )
+
+        file_path = facial_rigger.lib.get_file_path(self.filter, "save")
+        if not file_path:
+            return
+
+        with open(file_path, "w") as f:
+            f.write(data_string)
+
+    def import_settings(self):
+        file_path = facial_rigger.lib.get_file_path(self.filter, "open")
+        if not file_path:
+            return
+
+        facial_rigger.lib.import_settings_from_file(file_path, self)
+
+
+# Build from json file.
+def rig_from_file(path):
+    rig(**json.load(open(path)))
+
+
+def show(*args):
+    gqt.showDialog(ui)
